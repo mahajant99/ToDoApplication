b5b72547da0110082492f404e290a7536850f20c
Tushar Mahajan <mahajant99@gmail.com>
2024-02-14 12:12:43 +0530

    ToDoApplication: Initial commit - Springboot structure
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

---
+++ src/test/java/com/mahajant99/todoapplication/TodoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahajant99.todoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
