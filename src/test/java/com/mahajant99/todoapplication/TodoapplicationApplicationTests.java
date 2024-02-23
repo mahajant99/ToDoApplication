eb941411d40c91e1093f82abc2a0760a4b5497b4
Tushar Mahajan <mahajant99@gmail.com>
2024-02-23 13:23:57 +0530

    ToDoApplication: maven ==> gradle
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/test/java/com/mahajant99/todoapplication/TodoapplicationApplicationTests.java
+++ src/test/java/com/mahajant99/todoapplication/TodoapplicationApplicationTests.java
@@ -1,13 +1,9 @@
 package com.mahajant99.todoapplication;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TodoapplicationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
